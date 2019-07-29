5ba6af9c4d642bb1f6703670265b07a802a44f8e
luongmax97 <53015611+luongmax97@users.noreply.github.com>
2019-07-29 07:31:25 +0700

    Add/show current location

--- app/src/main/java/com/example/sharelocation/screens/MapActivity.java
+++ app/src/main/java/com/example/sharelocation/screens/MapActivity.java
@@ -1,23 +1,63 @@
 package com.example.sharelocation.screens;
 
+//import android.location.LocationListener;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.example.sharelocation.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.common.ConnectionResult;//lay thu vien nay sua
+import com.google.android.gms.location.LocationListener;//thay doi thu vien tren
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
-public class MapActivity extends BaseActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MapActivity extends BaseActivity implements
+        OnMapReadyCallback,
+        View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mGoogleMap;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
     private SearchView mSearchView;
     private DrawerLayout mDrawerLayout;
+    //them bien cho current location
+    private GoogleApiClient mgoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mlastLocation;
+    private Marker mcurrentUserLocationMarker;
+    private static final int Request_User_Location = 111;
+
+    View mapView;
 
     @Override
     protected int getLayoutResource() {
@@ -37,9 +77,32 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback, Vie
         mActionBarDrawerToggle.syncState();
     }
 
+
+    @SuppressLint("ResourceType")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //TODO: Consider Calling
+            buildGoogleApiClient();
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+        //di chuyen button vitri xuong duoi
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().
+                findFragmentById(R.id.fragment_map);
+        View mapView = mapFragment.getView();
+        if (mapView != null && mapView.findViewById(1) != null) {
+            // Get the button view
+            View locationButton = ((View) mapView.findViewById(1).getParent()).findViewById(2);
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
     }
 
     @Override
@@ -82,10 +145,97 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback, Vie
 
     private void initGoogleMap() {
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapFragment);
-
+                .findFragmentById(R.id.fragment_map);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkUserLocationPermission();
+        }
         if (supportMapFragment != null) {
             supportMapFragment.getMapAsync(this);
         }
+    }
+
+    public boolean checkUserLocationPermission(){//check  gtri location
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location);
+            }else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location);
+            }
+
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case Request_User_Location:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED){
+                        if(mgoogleApiClient == null){
+                            buildGoogleApiClient();
+                        }
+                        mGoogleMap.setMyLocationEnabled(true);
+                    }
+                }else{
+                    Toast.makeText(this, "permission denied...",Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        mgoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mgoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1100);
+        mLocationRequest.setFastestInterval(1100);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mgoogleApiClient,mLocationRequest, this);
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mlastLocation = location;
+        if(mcurrentUserLocationMarker != null){
+            mcurrentUserLocationMarker.remove();
+        }
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("i am here");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        mcurrentUserLocationMarker = mGoogleMap.addMarker(markerOptions);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomBy(14));
+
+        if(mgoogleApiClient != null){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mgoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Lỗi kết nối: " + connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
     }
 }
